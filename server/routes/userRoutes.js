81a174e98ca4df87b3b1c0eb0283e9d2054f70c7
adam <adam.fariz@um5r.ac.ma>
2026-05-01 00:57:14 +0100

    feat: expose profile and teacher statistics endpoints

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { verifyToken } = require('../middleware/auth'); 
-const { getUserInfo, updateUserInfo, deleteUser, enrollCourse,enrollLesson, getAllCoursesOfInstructor,getAllCoursesEnrolled } = require('../controllers/user');
+const { getUserInfo, updateUserInfo, deleteUser, enrollCourse,enrollLesson, getAllCoursesOfInstructor,getAllCoursesEnrolled, updateUserProfile, getPublicProfile } = require('../controllers/user');
 
 router.get('/me', verifyToken, getUserInfo);
 router.put('/me', verifyToken, updateUserInfo);
@@ -10,5 +10,10 @@ router.post('/enroll-course/:courseId', verifyToken, enrollCourse);
 router.post('/enroll-lesson/:lessonId', verifyToken, enrollLesson);
 router.get('/teacher/courses', verifyToken, getAllCoursesOfInstructor);
 router.get('/student/courses', verifyToken, getAllCoursesEnrolled);
+
+// New endpoints
+router.get('/me/courses', verifyToken, getAllCoursesEnrolled);
+router.put('/profile', verifyToken, updateUserProfile);
+router.get('/:id/public', getPublicProfile);
 
 module.exports = router;
